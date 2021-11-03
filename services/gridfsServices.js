56469b0be5505739889e92a6d0d98a5fe84ba706
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-03 14:40:51 +0100

    fix: correct gridfs service to use db connection

--- services/gridfsServices.js
+++ services/gridfsServices.js
@@ -1,19 +1,9 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
 
-const conn = async function getDb() {
-  const dbName = 'kmt_sarl';
-  const dbUrl = 'mongodb://localhost:27017/';
-  const client = new mongoose.mongo.MongoClient(dbUrl);
-
-  await client.connect();
-  return client.db(dbName);
-};
-
 class GridFSService {
   uploadFile = async (filePath, filename, options) => {
-    const db = await conn();
-    const bucket = new mongoose.mongo.GridFSBucket(db);
+    const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
     const stream = await fs
       .createReadStream(filePath)
       .pipe(bucket.openUploadStream(filename, options));
