2c7429d93fabc7da617946f00c131690a1094b19
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-08 11:05:22 +0100

    feat: add signin and signup routes in authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
+router.post('/signin', async (req, res, next) => {
+  passport.authenticate('signin', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error('An error occurred.');
+        return next(error);
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const token = jwt.sign({ user }, 'TOP_SECRET', { expiresIn: '1d' });
+
+        return res.json({ user, token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
 router.post(
-  '/login',
-  passport.authenticate('local', { session: false }),
+  '/signup',
+  passport.authenticate('signup', { session: false }),
   (req, res) => {
-    res.json(req.user);
+    res.json({
+      message: 'Signup successful',
+      user: req.user,
+    });
   },
 );
 
