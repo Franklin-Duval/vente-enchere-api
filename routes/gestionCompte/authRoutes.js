7ec079cb0e605de9e104e041455a32eb1f797abd
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-08 15:44:59 +0100

    feat: update signup with passport js

--- routes/gestionCompte/authRoutes.js
+++ routes/gestionCompte/authRoutes.js
@@ -24,15 +24,26 @@ router.post('/signin', async (req, res, next) => {
   })(req, res, next);
 });
 
-router.post(
-  '/signup',
-  passport.authenticate('signup', { session: false }),
-  (req, res) => {
-    res.json({
-      message: 'Signup successful',
-      user: req.user,
-    });
-  },
-);
+router.post('/signup', async (req, res, next) => {
+  passport.authenticate('signup', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error('Bad request');
+        return res.status(400).json({ error: error.message });
+      }
+      console.log(req.body);
+      return res.json({ user });
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const token = jwt.sign({ user }, 'TOP_SECRET', { expiresIn: '1d' }); // token expiration of 1 day
+
+        return res.json({ user, token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
 
 module.exports = router;
