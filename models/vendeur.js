15fed3ae265af9b4769eefe70919a77623574695
cyrus937 <cyrillekenfack4@gmail.com>
2021-11-08 04:31:23 +0100

    feat: add model vendeur

---
+++ models/vendeur.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const COLLECTIONS = require('../database/collections');
+
+const Schema = mongoose.Schema;
+
+const VendeurSchema = new Schema({
+  nom: {
+    type: String,
+    required: true,
+  },
+  prenom: {
+    type: String,
+    required: true,
+  },
+  adresse: {
+    type: String,
+    required: true,
+  },
+  pays: {
+    type: String,
+    required: true,
+  },
+  ville: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  telephone: {
+    type: String,
+    required: true,
+  },
+  pseudo: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accreditation: {
+    type: Boolean,
+    required: true,
+  },
+  numeroCNI: {
+    type: String,
+    required: true,
+  },
+  specialite: {
+    type: String,
+    required: true,
+  },
+  chiffreAffaire: {
+    type: Number,
+    required: true,
+  },
+  nombreLotsVendu: {
+    type: Number,
+    required: true,
+  },
+  gerant: {
+    type: Schema.Types.ObjectId,
+    ref: COLLECTIONS.GERANTS,
+  },
+  dateAjout: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+const Vendeur = mongoose.model(COLLECTIONS.VENDEURS, VendeurSchema);
+
+module.exports = Vendeur;
