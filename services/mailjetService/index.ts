ab8c7fd4a9f2a43cf4ea460620f5f6e45e101080
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-08 16:10:23 +0100

    feat: create mailjet Service

---
+++ services/mailjetService/index.ts
@@ -0,0 +1,56 @@
+import { connect } from 'node-mailjet';
+
+const mailJet = connect(
+  '29f39b19e57a35dab3c0ce22b8c64123',
+  'bcbf2ffec5af4b3b0f33eedb08f8ad8f',
+);
+
+class MailJetService {
+  sendValidationEmail = (
+    receiver: { Email: string; Name: string },
+    activationLink: string,
+  ) => {
+    return this.sendMailWithTemplateId(
+      undefined,
+      [receiver],
+      3116376,
+      'Bienvenue sur Agric Auctions',
+      { activationLink },
+    );
+  };
+
+  async sendMailWithTemplateId(
+    sender = {
+      Email: 'franklinfrost14@gmail.com',
+      Name: 'AGRIC AUCTIONS - E-COM',
+    },
+    receivers: { Email: string; Name: string }[],
+    templateID: number,
+    subject: string,
+    variables = {},
+  ) {
+    const request = mailJet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: sender,
+          To: receivers,
+          Bcc: [sender],
+          TemplateID: templateID,
+          TemplateLanguage: true,
+          Subject: subject,
+          Variables: variables,
+        },
+      ],
+    });
+
+    return await request
+      .then((result) => {
+        return result.body;
+      })
+      .catch((err) => {
+        console.log(err, err.message);
+      });
+  }
+}
+
+module.exports = MailJetService;
