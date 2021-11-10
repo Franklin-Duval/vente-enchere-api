3a7b2e6c673a4b3c9588c9b03b4d35b2addf786d
cyrus937 <cyrillekenfack4@gmail.com>
2021-11-10 08:13:30 +0100

    feat: add fonction change activeOneCompte

--- controllers/gestionCompte/compteController.js
+++ controllers/gestionCompte/compteController.js
@@ -74,44 +74,61 @@ exports.updateOneCompte = (req, res, next) => {
     isActivated: false,
   }); */
 
-  Compte.updateOne(
-    { _id: req.params.id },
-    { email: req.body.email, isActivated: false },
-  )
-    .then(() => {
-      res.status(200).json({
-        success: true,
-        message: 'Le compte a été modifié avec succès',
-        result: { email: req.body.email, isActivated: false },
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({
-        success: false,
-        message: "Une erreur s'est produite",
-        result: undefined,
-      });
+  let compte = Compte.findOne({ _id: req.params.id });
+  console.log('idnkdgnkdng');
+  if (compte) {
+    //compte.email = req.body.email;
+    //compte.isActivated = false;
+
+    const compt = new Compte({
+      _id: req.params.id,
+      email: req.body.email,
+      password: compte.password,
+      isActivated: true,
     });
+
+    Compte.updateOne({ _id: req.params.id }, compt)
+      .then(() => {
+        res.status(200).json({
+          success: true,
+          message: 'Le compte a été modifié avec succès',
+          result: compte.email,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).json({
+          success: false,
+          message: "Une erreur s'est produite",
+          result: undefined,
+        });
+      });
+  }
 };
 
 exports.activateOneCompte = (req, res, next) => {
-  Compte.updateOne({ _id: req.params.id }, { isActivated: true })
-    .then(() => {
-      res.status(200).json({
-        success: true,
-        massage: 'Le compte a été activée',
-        result: true,
+  let compte = Compte.findOne({ _id: req.params.id });
+
+  if (compte) {
+    compte.isActivated = true;
+
+    Compte.updateOne({ _id: req.params.id }, compte)
+      .then(() => {
+        res.status(200).json({
+          success: true,
+          massage: 'Le compte a été activée',
+          result: true,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).json({
+          success: false,
+          message: "Une erreur s'est produite",
+          result: undefined,
+        });
       });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({
-        success: false,
-        message: "Une erreur s'est produite",
-        result: undefined,
-      });
-    });
+  }
 };
 
 exports.changePassword = async (req, res, next) => {
@@ -122,10 +139,8 @@ exports.changePassword = async (req, res, next) => {
         compte.password,
       );
       if (val) {
-        Compte.updateOne(
-          { _id: req.params.id },
-          { password: req.body.newPassword },
-        )
+        compte.password = EncryptionService.hashPassword(req.body.newPassword);
+        Compte.updateOne({ _id: req.params.id }, compte)
           .then(() => {
             res.status(200).json({
               success: true,
--- routes/gestionCompte/compteRoutes.js
+++ routes/gestionCompte/compteRoutes.js
@@ -6,9 +6,9 @@ const compteCtrl = require('../../controllers/gestionCompte/compteController');
 router.get('/', compteCtrl.getAllCompte);
 router.post('/', compteCtrl.createCompte);
 router.get('/:id', compteCtrl.getOneCompte);
-router.patch('/:id', compteCtrl.updateOneCompte);
-router.patch('/activecompte/:id', compteCtrl.activateOneCompte);
-router.patch('/changepassword/:id', compteCtrl.changePassword);
+router.put('/:id', compteCtrl.updateOneCompte);
+router.put('/activecompte/:id', compteCtrl.activateOneCompte);
+router.put('/changepassword/:id', compteCtrl.changePassword);
 router.delete('/:id', compteCtrl.deleteOneCompte);
 
 module.exports = router;
