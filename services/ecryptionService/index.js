35634d6a9d6fc92fe4f4862a730bbc30dee048df
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-11 08:01:28 +0100

    feat: update Encryption Service

--- services/ecryptionService/index.js
+++ services/ecryptionService/index.js
@@ -1,15 +1,5 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
-
-/* export class EncryptionService {
-  static async hashPassword(passwordToHash) {
-    return await bcrypt.hash(passwordToHash, saltRounds);
-  }
-
-  static async comparePassword(password, hashedPassword) {
-    return await bcrypt.compare(password, hashedPassword);
-  }
-} */
 
 exports.hashPassword = async (passwordToHash) => {
   return await bcrypt.hash(passwordToHash, saltRounds);
