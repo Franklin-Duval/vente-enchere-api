f46823fb93aadd1541eff13f3d6fc5516313d45a
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-15 11:13:21 +0100

    feat: update mailjet service

--- services/mailjetService/index.js
+++ services/mailjetService/index.js
@@ -1,53 +1,49 @@
-import { connect } from 'node-mailjet';
-
-const mailJet = connect(
-  '29f39b19e57a35dab3c0ce22b8c64123',
-  'bcbf2ffec5af4b3b0f33eedb08f8ad8f',
+const mailJet = require('node-mailjet').connect(
+  '1c8c4797d0f32db720cc0a70a6134015',
+  '772ee643fe53d1928c3b0300f970d851',
 );
 
-class MailJetService {
-  sendValidationEmail = (receiver, activationLink) => {
-    return this.sendMailWithTemplateId(
-      undefined,
-      [receiver],
-      3116376,
-      'Bienvenue sur Agric Auctions',
-      { activationLink },
-    );
-  };
+exports.sendValidationEmail = async (receiver, activationLink) => {
+  console.log(activationLink);
+  return await sendMailWithTemplateId(
+    undefined,
+    [receiver],
+    3325308,
+    'Bienvenue sur Agric Auctions',
+    { activationLink },
+  );
+};
 
-  async sendMailWithTemplateId(
-    sender = {
-      Email: 'franklinfrost14@gmail.com',
-      Name: 'AGRIC AUCTIONS - E-COM',
-    },
-    receivers,
-    templateID,
-    subject,
-    variables = {},
-  ) {
-    const request = mailJet.post('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: sender,
-          To: receivers,
-          Bcc: [sender],
-          TemplateID: templateID,
-          TemplateLanguage: true,
-          Subject: subject,
-          Variables: variables,
-        },
-      ],
+const sendMailWithTemplateId = async (
+  sender = {
+    Email: 'tmaevaleslie0202@gmail.com',
+    Name: 'AGRIC AUCTIONS - E-COM',
+  },
+  receivers,
+  templateID,
+  subject,
+  variables = {},
+) => {
+  console.log(sender, receivers, templateID, subject, variables);
+  const request = mailJet.post('send', { version: 'v3.1' }).request({
+    Messages: [
+      {
+        From: sender,
+        To: receivers,
+        Bcc: [sender],
+        TemplateID: templateID,
+        TemplateLanguage: true,
+        Subject: subject,
+        Variables: variables,
+      },
+    ],
+  });
+
+  return await request
+    .then((result) => {
+      return result.body;
+    })
+    .catch((err) => {
+      console.log(err, err.message);
     });
-
-    return await request
-      .then((result) => {
-        return result.body;
-      })
-      .catch((err) => {
-        console.log(err, err.message);
-      });
-  }
-}
-
-module.exports = MailJetService;
+};
